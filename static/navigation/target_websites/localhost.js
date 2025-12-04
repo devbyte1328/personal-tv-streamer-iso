c1191837645f8ebfc6ee8b81995cd32ece281e0a
devbyte1328 <devbyte1328@gmail.com>
2025-12-04 23:41:06 +0200

    Hardcode element selection in 'Home' page; hardcode element selection or pages on the left side

--- static/navigation/target_websites/localhost.js
+++ static/navigation/target_websites/localhost.js
@@ -4,22 +4,9 @@
   window.STNAV_TARGETS = window.STNAV_TARGETS || {};
 
   window.STNAV_TARGETS.selectors = [
-    'a[href]',
-    'button:not([disabled])',
-    'input:not([disabled])',
-    'select:not([disabled])',
-    'textarea:not([disabled])',
-    '[tabindex]:not([tabindex="-1"])',
-    '[contenteditable="true"]',
-    'ytd-thumbnail',
-    'ytd-playlist-panel-video-renderer',
-    '.yt-lockup-view-model',
-    'ytmusic-responsive-list-item-renderer .title',
-    'a.videostream__link.link',
-    'li',
-    '[role="button"]',
-    '[role="link"]',
-    'div.rgpl-btn-play'
+    '#sidebar ul li[data-url]',
+    '.home-video-wrapper iframe#standalone-home-video-frame',
+    '.home-video-wrapper #standalone-home-video-overlay'
   ];
 })();
 
