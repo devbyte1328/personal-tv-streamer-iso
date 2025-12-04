a30164b71c6aa9b0f5774ff95a0b55e28766e441
devbyte1328 <devbyte1328@gmail.com>
2025-12-04 08:15:25 +0200

    Add templates for hardcoding website navigation

---
+++ static/navigation/target_websites/youtube.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  window.STNAV_TARGETS = window.STNAV_TARGETS || {};
+
+  window.STNAV_TARGETS.selectors = [
+    'a[href]',
+    'button:not([disabled])',
+    'input:not([disabled])',
+    'select:not([disabled])',
+    'textarea:not([disabled])',
+    '[tabindex]:not([tabindex="-1"])',
+    '[contenteditable="true"]',
+    'ytd-thumbnail',
+    'ytd-playlist-panel-video-renderer',
+    '.yt-lockup-view-model',
+    'ytmusic-responsive-list-item-renderer .title',
+    'a.videostream__link.link',
+    'li',
+    '[role="button"]',
+    '[role="link"]',
+    'div.rgpl-btn-play'
+  ];
+})();
+
