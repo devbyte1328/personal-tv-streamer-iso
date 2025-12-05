1a17224ae7a446fda8a490ba28dfa49d73a67576
Michael Ilgiaev <michaelilgiaev@zohomail.com>
2025-12-05 21:44:19 +0200

    Add 'Curated' page elements to the selectable elements list for navigation

--- static/navigation/target_websites/localhost.js
+++ static/navigation/target_websites/localhost.js
@@ -6,7 +6,17 @@
   window.STNAV_TARGETS.selectors = [
     '#sidebar ul li[data-url]',
     '.home-video-wrapper iframe#standalone-home-video-frame',
-    '.home-video-wrapper #standalone-home-video-overlay'
+    '.home-video-wrapper #standalone-home-video-overlay',
+    '#carousel-container iframe[data-position="center"]',
+    '#carousel-container iframe[data-position="left"]',
+    '#carousel-container iframe[data-position="right"]',
+    '.carousel-overlay',
+    '#standalone-trailer-frame',
+    '#standalone-trailer-overlay',
+    '.thumbnail-wrapper img',
+    '.thumbnail-wrapper .thumbnail-overlay',
+    '#left-carousel-btn',
+    '#right-carousel-btn'
   ];
 })();
 
