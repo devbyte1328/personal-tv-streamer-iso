b626e04de46c105b478fe9a7a85086e6eecbe0c1
devbyte1328 <devbyte1328@gmail.com>
2025-12-06 04:07:09 +0200

    Fix element selection staying on screen during scroll

--- static/navigation/focus.js
+++ static/navigation/focus.js
@@ -20,12 +20,13 @@
     const elementList = getFocusableList();
     if (!elementList.length) return;
 
-    const nextElement = findNextItem(state.activeElement || elementList[0], elementList, directionName);
+    const nextElement =
+      findNextItem(state.activeElement || elementList[0], elementList, directionName);
 
     if (!nextElement) {
       const scrollFactor =
         directionName === 'down' ? 1 :
-        directionName === 'up' ? -1 : 0;
+        directionName === 'up'   ? -1 : 0;
 
       if (scrollFactor !== 0) {
         window.scrollBy({
@@ -35,7 +36,8 @@
 
         setTimeout(() => {
           const updatedList = getFocusableList();
-          const updatedNext = findNextItem(state.activeElement, updatedList, directionName);
+          const updatedNext =
+            findNextItem(state.activeElement, updatedList, directionName);
           if (updatedNext) highlightAgain(updatedNext);
         }, 260);
 
@@ -47,5 +49,17 @@
   }
 
   window.STNAV_FOCUS = { move };
+
+  window.addEventListener('keydown', event => {
+    if (event.code === 'PageUp' || event.code === 'PageDown') {
+      state.isScrolling = true;
+      window.STNAV_CORE.clearHighlight();
+      clearTimeout(state._pageScrollDelay);
+      state._pageScrollDelay = setTimeout(() => {
+        state.isScrolling = false;
+        if (state.activeElement) updateOverlay(state.activeElement);
+      }, 180);
+    }
+  });
 })();
 
