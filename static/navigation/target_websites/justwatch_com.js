bb58a3a403532215ea6c6887a041aa9569731f8f
Michael Ilgiaev <michaelilgiaev@zohomail.com>
2025-12-11 06:22:45 +0200

    Hardcode JustWatch and SoundCloud for navigation

---
+++ static/navigation/target_websites/justwatch_com.js
@@ -0,0 +1,29 @@
+(function () {
+  'use strict';
+
+  window.STNAV_TARGETS = window.STNAV_TARGETS || {};
+
+  window.STNAV_TARGETS.selectors = [
+    'a[href]',
+    'button:not([disabled])',
+    'input:not([disabled])',
+    'select:not([disabled])',
+    'textarea:not([disabled])',
+    '[tabindex]:not([tabindex="-1"])',
+    '[contenteditable="true"]',
+    'ytd-thumbnail',
+    'ytd-playlist-panel-video-renderer',
+    '.yt-lockup-view-model',
+    'ytmusic-responsive-list-item-renderer .title',
+    'a.videostream__link.link',
+    'li',
+    '[role="button"]',
+    '[role="link"]',
+    'div.rgpl-btn-play',
+    '.title-detail-hero__backdrop__play',
+    '.title-detail-hero__backdrop__play__container',
+    'button.expand-btn',
+    'h4[role="button"]'
+  ];
+})();
+
