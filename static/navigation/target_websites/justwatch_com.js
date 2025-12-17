a5f09a39fcd3cfe89cf31d0a4a3904865f13fe62
Michael Ilgiaev <michaelilgiaev@zohomail.com>
2025-12-17 14:00:10 +0200

    Fix navigation system for 'JustWatch'

--- static/navigation/target_websites/justwatch_com.js
+++ static/navigation/target_websites/justwatch_com.js
@@ -10,20 +10,7 @@
     'select:not([disabled])',
     'textarea:not([disabled])',
     '[tabindex]:not([tabindex="-1"])',
-    '[contenteditable="true"]',
-    'ytd-thumbnail',
-    'ytd-playlist-panel-video-renderer',
-    '.yt-lockup-view-model',
-    'ytmusic-responsive-list-item-renderer .title',
-    'a.videostream__link.link',
-    'li',
-    '[role="button"]',
-    '[role="link"]',
-    'div.rgpl-btn-play',
-    '.title-detail-hero__backdrop__play',
-    '.title-detail-hero__backdrop__play__container',
-    'button.expand-btn',
-    'h4[role="button"]'
+    '[contenteditable="true"]'
   ];
 })();
 
