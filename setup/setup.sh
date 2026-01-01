62ed738f4e736034e352eaa43a2ad750f4d4ea4c
devbyte1328 <devbyte1328@gmail.com>
2026-01-01 22:06:44 +0200

    Add missing dependancy for WebApp

--- setup/setup.sh
+++ setup/setup.sh
@@ -27,7 +27,7 @@ sudo -u tv-streamer bash -c 'cd /tmp/yay && makepkg -si --noconfirm >/dev/null 2
 sudo rm -rf /tmp/yay # ">/dev/null 2>&1" to make it silent
 
 echo "Installing packages, this takes a moment..."
-sudo -u tv-streamer bash -c "yay -S --noconfirm --needed curl ffmpeg python python-pip gedit librewolf-bin >/dev/null 2>&1" # ">/dev/null 2>&1" to make it silent, "grep -v '^\s*#'" to ignore the comments
+sudo -u tv-streamer bash -c "yay -S --noconfirm --needed curl ffmpeg python python-pip tk gedit librewolf-bin >/dev/null 2>&1" # ">/dev/null 2>&1" to make it silent, "grep -v '^\s*#'" to ignore the comments
 
 echo "Removing Manjaro Cinnamon packages that are not needed, this takes a moment..."
 sudo pacman -R --noconfirm webapp-manager vivaldi timeshift thunderbird xed gnome-screenshot system-config-printer pix onboard mpv micro manjaro-hello manjaro-application-utility galculator bulky gparted lollypop xviewer-plugins xviewer yelp dconf-editor celluloid gimp gcolor3 pamac-gtk pamac-cli libpamac libpamac-flatpak-plugin # ">/dev/null 2>&1" to make it silent
