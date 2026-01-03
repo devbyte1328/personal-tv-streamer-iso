bed66316ebf86c16dc4f603fe428564cc8da212c
Michael Ilgiaev <michaelilgiaev@zohomail.com>
2026-01-03 16:25:13 +0200

    Modify URL change to visit Python Flask URL for loading spinner control

--- setup/loading-spinner.js
+++ setup/loading-spinner.js
@@ -1,8 +1,12 @@
 browser.webNavigation.onCommitted.addListener(d => {
-  if (d.frameId === 0) console.log("New URL:");
+  if (d.frameId === 0) {
+    fetch("http://localhost:8080/url-control-start-spinner", { method: "GET", keepalive: true });
+  }
 });
 
 browser.webNavigation.onCompleted.addListener(d => {
-  if (d.frameId === 0) console.log("Website finished loading");
+  if (d.frameId === 0) {
+    fetch("http://localhost:8080/url-control-stop-spinner", { method: "GET", keepalive: true });
+  }
 });
 
