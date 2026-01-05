1f93205ed99138005d9c9a6007cc400993c19d47
Michael Ilgiaev <michaelilgiaev@zohomail.com>
2026-01-05 02:34:45 +0200

    Fix install script to run cleanly with sudo, user-scoped config, and Python venv setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -55,3 +55,5 @@ cp Xmodmap /home/tv-streamer/.Xmodmap
 chown tv-streamer:tv-streamer /home/tv-streamer/.Xmodmap
 sudo -u tv-streamer xmodmap /home/tv-streamer/.Xmodmap
 
+echo "Setting up Python virtual environment and installing requirements..."
+sudo -u tv-streamer bash -c 'cd .. && python -m venv venv >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent && source venv/bin/activate >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent && pip install -r requirements.txt >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent' # ">/dev/null 2>&1" to make it silent
