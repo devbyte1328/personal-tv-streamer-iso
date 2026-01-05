4e6d51c13b3bccfa8a858084baeb7f53b86252b5
Michael Ilgiaev <michaelilgiaev@zohomail.com>
2026-01-05 02:15:50 +0200

    Fix commit(2c1c438)

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,45 +5,53 @@ if [ "$(id -u)" -ne 0 ]; then
   exit 1
 fi
 
+set -e
+
 echo "Removing sudo password for 'tv-streamer'"
-echo "tv-streamer ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/90-nopasswd-tv-streamer
-sudo chmod 440 /etc/sudoers.d/90-nopasswd-tv-streamer
-sudo visudo -c > /dev/null # ">/dev/null" to make it silent
+echo "tv-streamer ALL=(ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/90-nopasswd-tv-streamer
+chmod 440 /etc/sudoers.d/90-nopasswd-tv-streamer
+visudo -c > /dev/null
 
 echo "Resolve errors caused by upstream (discovered on 1/1/2026)"
-yes | sudo pacman -Sy archlinux-keyring manjaro-keyring >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
-yes | sudo pacman-key --init >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
-yes | sudo pacman-key --populate archlinux manjaro >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
-yes | sudo pacman -Scc >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
-sudo pacman -Rdd --noconfirm cinnamon caribou >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+yes | pacman -Sy archlinux-keyring manjaro-keyring >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+pacman-key --init >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+pacman-key --populate archlinux manjaro >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+yes | pacman -Scc >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+pacman -Rdd --noconfirm cinnamon caribou >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
 
 echo "Updating package index and packages, this takes a moment..."
-sudo pacman -Syu --noconfirm >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+pacman -Syu --noconfirm >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
 
 echo "Installing Yay AUR helper, this takes a moment..."
 pacman -S --needed --noconfirm base-devel git >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
+rm -rf /tmp/yay
 sudo -u tv-streamer bash -c 'git clone https://aur.archlinux.org/yay.git /tmp/yay >/dev/null 2>&1' # ">/dev/null 2>&1" to make it silent
 sudo -u tv-streamer bash -c 'cd /tmp/yay && makepkg -si --noconfirm >/dev/null 2>&1' # ">/dev/null 2>&1" to make it silent
-sudo rm -rf /tmp/yay # ">/dev/null 2>&1" to make it silent
+rm -rf /tmp/yay # ">/dev/null 2>&1" to make it silent
 
 echo "Installing packages, this takes a moment..."
 sudo -u tv-streamer bash -c "yay -S --noconfirm --needed curl ffmpeg mpg123 python python-pip tk gedit librewolf-bin >/dev/null 2>&1" # ">/dev/null 2>&1" to make it silent, "grep -v '^\s*#'" to ignore the comments
 
 echo "Removing Manjaro Cinnamon packages that are not needed, this takes a moment..."
-sudo pacman -R --noconfirm webapp-manager vivaldi timeshift thunderbird xed gnome-screenshot system-config-printer pix onboard mpv micro manjaro-hello manjaro-application-utility galculator bulky gparted lollypop xviewer-plugins xviewer yelp dconf-editor celluloid gimp gcolor3 pamac-gtk pamac-cli libpamac libpamac-flatpak-plugin # ">/dev/null 2>&1" to make it silent
+pacman -R --noconfirm webapp-manager vivaldi timeshift thunderbird xed gnome-screenshot system-config-printer pix onboard mpv micro manjaro-hello manjaro-application-utility galculator bulky gparted lollypop xviewer-plugins xviewer yelp dconf-editor celluloid gimp gcolor3 pamac-gtk pamac-cli libpamac libpamac-flatpak-plugin >/dev/null 2>&1 # ">/dev/null 2>&1" to make it silent
 
 echo "Configuring Librewolf policies"
-sudo cp policies.json /usr/lib/librewolf/distribution/policies.json
+cp policies.json /usr/lib/librewolf/distribution/policies.json
 
 echo "Configuring Librewolf loading status"
 # Remove default grey colored new URL display at bottom left
-mkdir -p ~/.librewolf/*.default-default/chrome
-cp userChrome.css ~/.librewolf/*.default-default/chrome/
+mkdir -p /home/tv-streamer/.librewolf
+mkdir -p /home/tv-streamer/.librewolf/*.default-default/chrome
+cp userChrome.css /home/tv-streamer/.librewolf/*.default-default/chrome/
+chown -R tv-streamer:tv-streamer /home/tv-streamer/.librewolf
 # Add loading screen connector to Python 
+rm -f loading-spinner@local.xpi
 zip -r loading-spinner@local.xpi manifest.json loading-spinner.js
-sudo mkdir -p /usr/lib/librewolf/browser/extensions
-sudo cp loading-spinner@local.xpi /usr/lib/librewolf/browser/extensions/loading-spinner@local.xpi
+mkdir -p /usr/lib/librewolf/browser/extensions
+cp loading-spinner@local.xpi /usr/lib/librewolf/browser/extensions/loading-spinner@local.xpi
 
 echo "Remapping keys for airmouse"
-sudo cp Xmodmap ~/.Xmodmap
-sudo xmodmap ~/.Xmodmap
+cp Xmodmap /home/tv-streamer/.Xmodmap
+chown tv-streamer:tv-streamer /home/tv-streamer/.Xmodmap
+sudo -u tv-streamer xmodmap /home/tv-streamer/.Xmodmap
+
